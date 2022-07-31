ef0f6e72d8c57501cd308a679ecca3bb810e25da
Eddie <99931005+eddiethenooblol@users.noreply.github.com>
2022-07-30 23:54:34 -0700

    i tried rigging this dice to troll my friends lol

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var randomNumber1 = 2;
+var randomNumber1 = Math.floor(Math.random()*6)+1;
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-var randomNumber2 = 6;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if(randomNumber1 === randomNumber2){
@@ -17,10 +17,7 @@ else{
 }
 /*
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-
 var randomImageSource = "images/" + randomDiceImage;
-
 var image1 = document.querySelectorAll("img")[0];
-
 image1.setAttribute("src", randomImageSource);
 */
