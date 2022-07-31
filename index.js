d1f8624115b1c608696563b36445025757a7fb82
Eddie <99931005+eddiethenooblol@users.noreply.github.com>
2022-07-30 23:46:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber1 = 2;
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = 6;
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if(randomNumber1 === randomNumber2){
